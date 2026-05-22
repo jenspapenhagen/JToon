317b15b38f3b10ce4c60ec8c340a69e44d9dd537
Jens Papenhagen <jens.papenhagen@web.de>
2026-05-22 17:51:03 +0200

    remove checkstyle warnings in test

---
+++ src/test/java/dev/toonformat/jtoon/BenchmarkConstants.java
@@ -0,0 +1,10 @@
+package dev.toonformat.jtoon;
+
+final class BenchmarkConstants {
+
+    static final int WARMUP_ITERATIONS = 3;
+    static final int MEASUREMENT_ITERATIONS = 5;
+
+    private BenchmarkConstants() {
+    }
+}
